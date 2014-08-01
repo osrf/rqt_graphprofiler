5c88323705aa1753cd2d78cd1ea871560139d312
dan brooks <dan@danbrooks.net>
2014-08-01 15:28:47 -0700

    [db] removed re-entrant lock from CallbackTimer - it wasnt safe. Fixed restart

--- scripts/callback_timer.py
+++ scripts/callback_timer.py
@@ -1,6 +1,7 @@
 from threading import *
 import sys
 import time
+import types
 
 class CallbackTimer(object):
     """ Callback timer
@@ -9,31 +10,48 @@ class CallbackTimer(object):
     def __init__(self, time, callback):
         self._timeLength = time
         self._user_callback = callback
-        self._lock = RLock()
+        self._lock = Lock()
         self._timer = None
+        # variable that says if we are runnign the callback
+        self._running_callback_flag = False
 
     def restart(self):
-        with self._lock:
-            self.stop()
-            return self.start()
+        has_lock = False
+        while (not has_lock) and (not self._running_callback_flag):
+            has_lock = self._lock.acquire(False)
+        self._stop()
+        t = self._start()
+        if has_lock:
+            self._lock.release()
+        return t
 
     def start(self):
         with self._lock:
-            self._timer = Timer(self._timeLength, self._callback)
-            self._timer.start()
-            return time.time()
+            return self._start()
 
     def stop(self):
         with self._lock:
-            if self._timer is not None:
-                self._timer.cancel()
-                self._timer = None
-            return time.time()
+            return self._stop()
+
+    def _start(self):
+        if not isinstance(self._timer,types.NoneType):
+            raise Exception("Timer already started")
+        self._timer = Timer(self._timeLength, self._callback)
+        self._timer.start()
+        return time.time()
+
+    def _stop(self):
+        if self._timer is not None:
+            self._timer.cancel()
+        self._timer = None
+        return time.time()
 
     def _callback(self):
         with self._lock:
+            self._running_callback_flag = True
             try:
                 self._user_callback()
             except:
                 import traceback
                 traceback.print_exc()
+            self._running_callback_flag = False
