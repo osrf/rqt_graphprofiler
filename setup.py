865bcec28657b5379731b565efb941689f97229c
dan brooks <dan@danbrooks.net>
2014-08-14 17:36:42 -0700

    solves diarc not being installed properly. fixes #23

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from distutils.core import setup
 from catkin_pkg.python_setup import generate_distutils_setup
 
 d = generate_distutils_setup(
-        packages=['diarc'],
+        packages=['diarc','diarc.diarc', 'diarc.qt_view', 'diarc.ascii_view', 'diarc.ros'],
         package_dir={'': 'src'}
         )
 
