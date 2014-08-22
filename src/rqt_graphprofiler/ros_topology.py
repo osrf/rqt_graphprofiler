e48aae0516befc84b584796627d3caa91093bc85
dan brooks <dan@danbrooks.net>
2014-08-22 14:47:22 -0700

    adds apache2 license

--- src/rqt_graphprofiler/ros_topology.py
+++ src/rqt_graphprofiler/ros_topology.py
@@ -1,15 +1,28 @@
-# [db] dan@danbrooks.net
+# Copyright 2014 Open Source Robotics Foundation, Inc.
 #
-# diarc topology data structures for ROS
-# This renames some things into ROS terminology to be more conveient, and also
-# adds some attributes we want to track. 
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
 #
-# Renaming Looks like this
-#   Vertex = Node
-#   Edge = Topic
-#   Sink = Subscriber
-#   Source = Publisher
+#     http://www.apache.org/licenses/LICENSE-2.0
 #
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+"""
+diarc topology data structures for ROS
+This renames some things into ROS terminology to be more conveient, and also
+adds some attributes we want to track. 
+
+Renaming Looks like this
+  Vertex = Node
+  Edge = Topic
+  Sink = Subscriber
+  Source = Publisher
+"""
+
 from diarc.topology import Topology
 from diarc.topology import Vertex
 from diarc.topology import Edge
